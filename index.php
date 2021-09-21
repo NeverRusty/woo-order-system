aa91e0c274014b860c6ff798927838689f9a5c4f
NeverRusty <jorge.rzpz@gmail.com>
2021-09-20 22:37:19 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ use Automattic\WooCommerce\Client;
 
 $woocommerce = new Client(
     'https://ventasbaires.com.ar/', 
-    'ck_8186e3bbe43b0a6f4bfdaa4d287ef2356b3a559c', 
-    'cs_7516dbbddfbae49beb22152e796bae7312f8764f',
+    'ck_X', 
+    'cs_X',
     [
         'version' => 'wc/v3',
     ]
